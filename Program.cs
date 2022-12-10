3b7a5d1f9da468abbfc53ceeef8274b37beba4e8
hannmx <mxkud@mail.ru>
2022-12-11 00:56:25 +0300

    Create prog

--- Program.cs
+++ Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿Console.Write("Введите первое натуральное число: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе натуральное число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+void SumNuber(int M)
+{
+    sum += M;
+    M++;
+    if(M > N)
+    {
+        Console.Write($"Сумма натуральных чисел: {sum}");
+        return;
+    }
+    SumNuber(M);
+}
+SumNuber(M);
